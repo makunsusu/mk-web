ea4d6144044fa80ec7819e2afded6abb02a52047
makun <540245405@qq.com>
2017-09-25 22:33:21 +0800

    Initial commit

---
+++ src/main/java/com/mk/mkweb/MkWebApplication.java
@@ -0,0 +1,12 @@
+package com.mk.mkweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkWebApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mk/mkweb/MkWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mk.mkweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MkWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
